2a81352d58d76968994ab93a893debc37ee91c92
Von <haoliweiwork@gmail.com>
2022-03-13 10:42:38 +0800

    [+]add libxml build shell script

---
+++ android/contrib/tools/do-compile-libxml.sh
@@ -0,0 +1,226 @@
+#! /usr/bin/env bash
+#
+# Copyright (C) 2014 Miguel Botón <waninkoko@gmail.com>
+# Copyright (C) 2014 Zhang Rui <bbcallen@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#--------------------
+set -e
+
+if [ -z "$ANDROID_NDK" ]; then
+    echo "You must define ANDROID_NDK before starting."
+    echo "They must point to your NDK directories.\n"
+    exit 1
+fi
+
+#--------------------
+# common defines
+FF_ARCH=$1
+if [ -z "$FF_ARCH" ]; then
+    echo "You must specific an architecture 'arm, armv7a, x86, ...'.\n"
+    exit 1
+fi
+
+
+FF_BUILD_ROOT=`pwd`
+FF_ANDROID_PLATFORM=android-9
+
+
+FF_BUILD_NAME=
+FF_SOURCE=
+FF_CROSS_PREFIX=
+
+FF_CFG_FLAGS=
+FF_PLATFORM_CFG_FLAGS=
+
+FF_EXTRA_CFLAGS=
+FF_EXTRA_LDFLAGS=
+
+
+
+#--------------------
+echo ""
+echo "--------------------"
+echo "[*] make NDK standalone toolchain"
+echo "--------------------"
+. ./tools/do-detect-env.sh
+FF_MAKE_TOOLCHAIN_FLAGS=$IJK_MAKE_TOOLCHAIN_FLAGS
+FF_MAKE_FLAGS=$IJK_MAKE_FLAG
+FF_GCC_VER=$IJK_GCC_VER
+FF_GCC_64_VER=$IJK_GCC_64_VER
+
+
+#----- armv7a begin -----
+if [ "$FF_ARCH" = "armv7a" ]; then
+    FF_BUILD_NAME=libxml-armv7a
+    FF_SOURCE=$FF_BUILD_ROOT/$FF_BUILD_NAME
+	
+    FF_CROSS_PREFIX=arm-linux-androideabi
+	FF_TOOLCHAIN_NAME=${FF_CROSS_PREFIX}-${FF_GCC_VER}
+
+    FF_PLATFORM_CFG_FLAGS="android-armv7"
+
+elif [ "$FF_ARCH" = "armv5" ]; then
+    FF_BUILD_NAME=libxml-armv5
+    FF_SOURCE=$FF_BUILD_ROOT/$FF_BUILD_NAME
+	
+    FF_CROSS_PREFIX=arm-linux-androideabi
+	FF_TOOLCHAIN_NAME=${FF_CROSS_PREFIX}-${FF_GCC_VER}
+
+    FF_PLATFORM_CFG_FLAGS="android"
+
+elif [ "$FF_ARCH" = "x86" ]; then
+    FF_BUILD_NAME=libxml-x86
+    FF_SOURCE=$FF_BUILD_ROOT/$FF_BUILD_NAME
+	
+    FF_CROSS_PREFIX=i686-linux-android
+	FF_TOOLCHAIN_NAME=x86-${FF_GCC_VER}
+
+    FF_PLATFORM_CFG_FLAGS="android-x86"
+
+    FF_CFG_FLAGS="$FF_CFG_FLAGS no-asm"
+
+elif [ "$FF_ARCH" = "x86_64" ]; then
+    FF_ANDROID_PLATFORM=android-21
+
+    FF_BUILD_NAME=libxml-x86_64
+    FF_SOURCE=$FF_BUILD_ROOT/$FF_BUILD_NAME
+
+    FF_CROSS_PREFIX=x86_64-linux-android
+    FF_TOOLCHAIN_NAME=${FF_CROSS_PREFIX}-${FF_GCC_64_VER}
+
+    FF_PLATFORM_CFG_FLAGS="linux-x86_64"
+
+elif [ "$FF_ARCH" = "arm64" ]; then
+    FF_ANDROID_PLATFORM=android-21
+
+    FF_BUILD_NAME=libxml-arm64
+    FF_SOURCE=$FF_BUILD_ROOT/$FF_BUILD_NAME
+
+    FF_CROSS_PREFIX=aarch64-linux-android
+    FF_TOOLCHAIN_NAME=${FF_CROSS_PREFIX}-${FF_GCC_64_VER}
+
+    FF_PLATFORM_CFG_FLAGS="linux-aarch64"
+
+else
+    echo "unknown architecture $FF_ARCH";
+    exit 1
+fi
+
+FF_TOOLCHAIN_PATH=$FF_BUILD_ROOT/build/$FF_BUILD_NAME/toolchain
+
+FF_SYSROOT=$FF_TOOLCHAIN_PATH/sysroot
+FF_PREFIX=$FF_BUILD_ROOT/build/$FF_BUILD_NAME/output
+
+mkdir -p $FF_PREFIX
+# mkdir -p $FF_SYSROOT
+
+
+#--------------------
+echo ""
+echo "--------------------"
+echo "[*] make NDK standalone toolchain"
+echo "--------------------"
+. ./tools/do-detect-env.sh
+FF_MAKE_TOOLCHAIN_FLAGS=$IJK_MAKE_TOOLCHAIN_FLAGS
+FF_MAKE_FLAGS=$IJK_MAKE_FLAG
+
+
+FF_MAKE_TOOLCHAIN_FLAGS="$FF_MAKE_TOOLCHAIN_FLAGS --install-dir=$FF_TOOLCHAIN_PATH"
+FF_TOOLCHAIN_TOUCH="$FF_TOOLCHAIN_PATH/touch"
+if [ ! -f "$FF_TOOLCHAIN_TOUCH" ]; then
+    $ANDROID_NDK/build/tools/make-standalone-toolchain.sh \
+        $FF_MAKE_TOOLCHAIN_FLAGS \
+        --platform=$FF_ANDROID_PLATFORM \
+        --toolchain=$FF_TOOLCHAIN_NAME
+    touch $FF_TOOLCHAIN_TOUCH;
+fi
+
+
+#--------------------
+echo ""
+echo "--------------------"
+echo "[*] check libxml env"
+echo "--------------------"
+export PATH=$FF_TOOLCHAIN_PATH/bin:$PATH
+
+export COMMON_FF_CFG_FLAGS=
+
+FF_CFG_FLAGS="$FF_CFG_FLAGS $COMMON_FF_CFG_FLAGS"
+
+#--------------------
+# Standard options:
+FF_CFG_FLAGS="$FF_CFG_FLAGS zlib-dynamic"
+FF_CFG_FLAGS="$FF_CFG_FLAGS no-shared"
+FF_CFG_FLAGS="$FF_CFG_FLAGS --libxmldir=$FF_PREFIX"
+FF_CFG_FLAGS="$FF_CFG_FLAGS --cross-compile-prefix=${FF_CROSS_PREFIX}-"
+FF_CFG_FLAGS="$FF_CFG_FLAGS $FF_PLATFORM_CFG_FLAGS"
+
+#--------------------
+echo ""
+echo "--------------------"
+echo "[*] compile libxml"
+echo "--------------------"
+NDK_ROOT=$ANDROID_NDK_HOME
+export NDK=$ANDROID_NDK_HOME
+
+export CROSS_COMPILE=$FF_CROSS_PREFIX
+export SYSROOT=$FF_SYSROOT
+
+export NDK_BUILD_BIN="${FF_TOOLCHAIN_PATH}/${CROSS_COMPILE}-4.9/prebuilt/linux-x86_64/bin"
+export PATH="$PATH:$NDK_BUILD_BIN"
+export CC="${CROSS_COMPILE}-gcc"
+export CXX="${CROSS_COMPILE}-g++"
+
+export CPPFLAGS="--sysroot=${SYSROOT}"
+export CFLAGS="--sysroot=${SYSROOT} -I."
+
+echo "-------------------------"
+echo "start configure"
+echo "-------------------------"
+
+cd ${FF_SOURCE}
+
+echo FF_SOURCE ${FF_SOURCE}
+
+echo FF_PREFIX ${FF_PREFIX}
+
+echo CROSS_COMPILE $CROSS_COMPILE
+
+
+if [ ! -f "${FF_SOURCE}/configure" ]; then
+    # configure文件不存在需要构造
+    ${FF_SOURCE}/autogen.sh
+fi
+
+${FF_SOURCE}/configure --build=X86_64-unknown-linux-gnu --host=${CROSS_COMPILE} --target=${CROSS_COMPILE} \
+--prefix=${FF_PREFIX} --without-python --without-zlib
+
+echo "-------------------------"
+echo "start make"
+echo "-------------------------"
+make -j8
+
+
+echo "-------------------------"
+echo "start install"
+echo "-------------------------"
+make install
+
+#--------------------
+echo ""
+echo "--------------------"
+echo "[*] link libxml"
+echo "--------------------"
